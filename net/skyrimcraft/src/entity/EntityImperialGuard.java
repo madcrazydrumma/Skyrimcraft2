bb5db9d1090c85f0b428ee22c9755e381b54ca2a
sheenrox82 <sheenrox82@yahoo.com>
2013-09-30 21:15:32 -0500

    Imperials now hold weapons and wear armor.

--- net/skyrimcraft/src/entity/EntityImperialGuard.java
+++ net/skyrimcraft/src/entity/EntityImperialGuard.java
@@ -4,21 +4,16 @@ import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+import net.skyrimcraft.src.content.SkyrimItems;
 
 public class EntityImperialGuard extends EntityMob
 {
 	public EntityImperialGuard(World par1World) {
 		super(par1World);
 		setSize(1.0F, 1.75F);
-		//if player's faction is stormcloak, attack...
-	}
-	
-	protected void addRandomArmor()
-    {
-        this.setCurrentItemOrArmor(0, new ItemStack(Item.swordIron)); //imperial sword...
-        /*this.setCurrentItemOrArmor(1, new ItemStack(SkyrimItems.imperialHelmet));
+        this.setCurrentItemOrArmor(1, new ItemStack(SkyrimItems.imperialHelmet));
         this.setCurrentItemOrArmor(2, new ItemStack(SkyrimItems.imperialChestplate));
         this.setCurrentItemOrArmor(3, new ItemStack(SkyrimItems.imperialLeggings));
-        this.setCurrentItemOrArmor(4, new ItemStack(SkyrimItems.imperialBoots));*/
-    }
+        this.setCurrentItemOrArmor(4, new ItemStack(SkyrimItems.imperialBoots));
+	}
 }
